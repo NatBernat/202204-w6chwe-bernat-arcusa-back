f60f162bc113c8ee95fddc071f7292ffeb48ce96
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-14 11:03:37 +0200

    Added errors test

---
+++ server/middlewares/errors.test.js
@@ -0,0 +1,51 @@
+const { notFoundError, generalError } = require("./errors");
+
+describe("Given a notFoundError function", () => {
+  describe("When its invoked with a response", () => {
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    test("Then it should call the reponse's method status with a 404", () => {
+      const expectedStatusCode = 404;
+
+      notFoundError(null, res);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+    test("Then it should call the reponse's method json with a message 'Any endpoint found", () => {
+      const expectedResponseMEssage = { msg: "Any endpoint found" };
+
+      notFoundError(null, res);
+
+      expect(res.json).toHaveBeenCalledWith(expectedResponseMEssage);
+    });
+  });
+});
+
+describe("Given a generalError function", () => {
+  describe("When its invoked with a response", () => {
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    const error = { msg: "Unknown error" };
+
+    test("Then it should call the reponse's method status with a 500", () => {
+      const expectedStatusCode = 500;
+
+      generalError(error, null, res);
+
+      expect(res.status).toHaveBeenCalledWith(expectedStatusCode);
+    });
+    test("Then it should call the reponse's method json with a message 'Server error'", () => {
+      const expectedResponseMessage = { msg: "Server error" };
+
+      generalError(error, null, res);
+
+      expect(res.json).toHaveBeenCalledWith(expectedResponseMessage);
+    });
+  });
+});
