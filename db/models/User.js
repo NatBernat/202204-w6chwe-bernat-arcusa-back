1bddd0feb057486237a09c9f51cb53fcd8f203d1
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-17 18:07:36 +0200

    Added User schema

---
+++ db/models/User.js
@@ -0,0 +1,16 @@
+const { Schema } = require("mongoose");
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", UserSchema, "users");
+
+module.exports = User;
